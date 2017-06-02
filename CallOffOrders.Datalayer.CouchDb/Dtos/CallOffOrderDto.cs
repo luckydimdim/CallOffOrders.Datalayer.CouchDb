8ff03a0060be1b84dc2a353e1b490651b6c310a5
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-06-02 16:30:39 +0300

    validation

--- CallOffOrders.Datalayer.CouchDb/Dtos/CallOffOrderDto.cs
+++ CallOffOrders.Datalayer.CouchDb/Dtos/CallOffOrderDto.cs
@@ -46,12 +46,12 @@ namespace Cmas.DataLayers.CouchDb.CallOffOrders.Dtos
         /// <summary>
         /// Дата начала действия наряд-заказа
         /// </summary>
-        public DateTime? StartDate;
+        public DateTime StartDate;
 
         /// <summary>
         /// Дата окончания действия наряд-заказа
         /// </summary>
-        public DateTime? FinishDate;
+        public DateTime FinishDate;
 
         /// <summary>
         /// Наименование заказа (по сути - работы)
