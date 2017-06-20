f8e917a330ae83e8bf027eedad03f8b758ed0d4a
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-06-20 09:14:30 +0300

    access refactoring

--- CallOffOrders.Datalayer.CouchDb/Queries/CountByContractIdQuery.cs
+++ CallOffOrders.Datalayer.CouchDb/Queries/CountByContractIdQuery.cs
@@ -12,37 +12,33 @@ using Cmas.DataLayers.Infrastructure;
 
 namespace Cmas.DataLayers.CouchDb.CallOffOrders.Queries
 {
-    public class FindByContractIdQuery : IQuery<FindByContractId, Task<IEnumerable<CallOffOrder>>>
+    /// <summary>
+    /// Получить количество НЗ по договору
+    /// </summary>
+    public class CountByContractIdQuery : IQuery<FindByContractId, Task<int>>
     {
         private readonly IMapper _autoMapper;
         private readonly CouchWrapper _couchWrapper;
 
-        public FindByContractIdQuery(IServiceProvider serviceProvider)
+        public CountByContractIdQuery(IServiceProvider serviceProvider)
         {
             _autoMapper = (IMapper)serviceProvider.GetService(typeof(IMapper));
 
             _couchWrapper = new CouchWrapper(serviceProvider, DbConsts.ServiceName);
         }
 
-        public async Task<IEnumerable<CallOffOrder>> Ask(FindByContractId criterion)
+        public async Task<int> Ask(FindByContractId criterion)
         {
-            var result = new List<CallOffOrder>();
-
             var query =
                 new QueryViewRequest(DbConsts.DesignDocumentName, DbConsts.ByContractDocsViewName).Configure(
-                    q => q.Key(criterion.ContractId).Reduce(false));
+                    q => q.Key(criterion.ContractId).Reduce(true).IncludeDocs(false));
 
             var viewResult = await _couchWrapper.GetResponseAsync(async (client) =>
             {
-                return await client.Views.QueryAsync<CallOffOrderDto>(query);
+                return await client.Views.QueryAsync(query);
             });
-
-            foreach (var row in viewResult.Rows.OrderByDescending(s => s.Value.CreatedAt))
-            {
-                result.Add(_autoMapper.Map<CallOffOrder>(row.Value));
-            }
-
-            return result;
+            
+            return viewResult.Rows.Length;
         }
     }
 }
